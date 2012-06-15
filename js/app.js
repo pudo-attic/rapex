eed7a00c8ab8cb1248b15fdf5e4a6b36ef28e625
Friedrich Lindenberg <friedrich@pudo.org>
2012-06-15 17:27:12 +0200

    Listing at the bottom

--- js/app.js
+++ js/app.js
@@ -9,23 +9,26 @@
     var selectedCategory = null, selectedCountry = null;
 
     var update = function() {
-        updateSidebar(ds.where({
-            rows: function(row) {
-                return !selectedCountry || selectedCountry==row.country;
-            }
-        }));
+        updateSidebar(ds);
         updateMap(ds.where({
             rows: function(row) {
                 return !selectedCategory || selectedCategory==row.descript_category;
             }
         }));
+        updateList(ds);
     };
 
     var updateSidebar = function(ds) {
-        var categories = ds.countBy('descript_category');
+        var categories = ds.where({
+            rows: function(row) {
+                return !selectedCountry || selectedCountry==row.country;
+            }
+        }).countBy('descript_category');
+
         categories = categories.sort({comparator: function(e, a) {
             return -1 * (e.count - a.count);
         }});
+        $("#nav li:has(a)").remove();
         categories.each(function(cat) {
             $("#nav").append("<li class='cat'><a href='#' data-name='"+cat.descript_category+"'>"+cat.descript_category+" (" +cat.count+ ")</a></li>");
         });
@@ -36,6 +39,27 @@
         var countries = ds.countBy('country');
         countries.each(function(country) {
             //console.log(country);
+        });
+    };
+
+    var updateList = function(ds) {
+        $("#list").empty();
+        if (!selectedCategory || !selectedCountry) {
+            return;
+        }
+        ds.where({
+            rows: function(row) {
+                return selectedCategory==row.descript_category &&
+                       selectedCountry==row.country;
+            }
+        }).each(function(row) {
+            $("#list").append("<tr> \
+                <td>"+row.descript_brand+"</td> \
+                <td>"+row.descript_product+"</td> \
+                <td>"+row.danger_short+"</td> \
+                <td>"+row.countryoforigin+"</td> \
+                <td>"+row.measures+"</td> \
+                </tr>");
         });
     };
 
@@ -52,6 +76,7 @@
             } else {
                 selectedCategory = categoryName;
             }
+            selectedCountry = 'Belgium';
             update();    
             $("#nav li").removeClass('active');
             $("#nav li").each(function(i, e) {
